f3e2aabf9600afa47987a2010f5838bd757950c7
Cv5 <mrchrist85@gmail.com>
2023-08-25 13:08:34 -0400

    Initial Commit

---
+++ Little Lemon's diners/Little_Lemon_s_dinersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_s_dinersApp.swift
+//  Little Lemon's diners
+//
+//  Created by Cristian  Veras on 25/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_s_dinersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
