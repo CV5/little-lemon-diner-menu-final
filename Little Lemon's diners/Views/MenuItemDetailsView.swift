d6ffcdf32c389d710a22b5cc46dd484853dd8833
Cv5 <mrchrist85@gmail.com>
2023-08-27 13:43:14 -0400

    My first commit of the Little lemons Diners app Avance Program META

---
+++ Little Lemon's diners/Views/MenuItemDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuItemDetailsView.swift
+//  Little Lemon's diners
+//
+//  Created by Cristian  Veras on 27/8/23.
+//
+
+import SwiftUI
+
+struct MenuItemDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuItemDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuItemDetailsView()
+    }
+}
